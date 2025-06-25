b82f396f68e80a4ea6c90b7f88a4a9e9820cffec
Shubhambn <shubhamnagare800@gmail.com>
2025-06-25 11:59:09 +0530

    Model Design Completed

---
+++ models/Video.ts
@@ -0,0 +1,40 @@
+import mongoose, {Schema} from "mongoose"
+
+export const Video_Dimension={
+    width:1080,
+    height:1090
+} as const
+
+export interface IVideo{
+    _id?:mongoose.Types.ObjectId;
+    title:string;
+    description:string;
+    videoURL:string;
+    thumbnailURL:string;
+    control?:boolean;
+    transformation:{
+        height:number,
+        width:number,
+        quality:number
+    }
+
+
+}
+
+const VideoSchema=new Schema<IVideo>({
+    title:{type:String,required:true},
+    description:{type:String,required:true},
+    videoURL:{type:String,required:true},
+    thumbnailURL:{type:String,required:true},
+    control:{type:Boolean,required:true},
+    transformation:{
+        height:{type:Number,default:Video_Dimension.height},
+        width:{type:Number,default:Video_Dimension.width},
+        quality:{type:Number,min:1,max:100}
+    }
+    
+},{timestamps:true})
+
+const Video = mongoose.models.VideoSchema||mongoose.model("User",VideoSchema)
+
+export default Video
